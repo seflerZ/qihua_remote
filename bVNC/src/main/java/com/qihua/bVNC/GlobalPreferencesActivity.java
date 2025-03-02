1f4957e5ab8dfb5ab1f04969c08ea911814f34a1
sefler <sefler@126.com>
2025-03-02 20:33:20 +0800

    修正一些文案。

--- bVNC/src/main/java/com/qihua/bVNC/GlobalPreferencesActivity.java
+++ bVNC/src/main/java/com/qihua/bVNC/GlobalPreferencesActivity.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.undatech.remoteClientUi.R;
+
 public class GlobalPreferencesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setTitle(getString(R.string.settings_default));
+
         getSupportFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new GlobalPreferencesFragment())
                 .commit();
